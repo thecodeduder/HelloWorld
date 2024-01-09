4f795ed1b0fe899e63c6058b05a708d4a0bd8f97
thecodeduder <78491938+thecodeduder@users.noreply.github.com>
2024-01-09 11:58:41 -0500

    Add files via upload

---
+++ MyFirstJavaProgram_DannyJones.java
@@ -0,0 +1,14 @@
+/**
+* My first project using java
+* 9 jan 2024
+* CSC 151 Tutorial 1 - My First Java Program
+*/
+public class MyFirstJavaProgram_DannyJones
+{
+	public static void main(String[] args)
+	{
+		System.out.println("Hello World!");
+	}
+}
+
+
